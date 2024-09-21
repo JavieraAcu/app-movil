fa0e7a8e48117ea8d59d2292df4280523c2a05f3
alanmartinez135 <ala.martinez@duocuc.cl>
2024-09-21 17:00:16 -0300

    se solucionan errores de inicio de sesion

--- src/app/pages/login/LoginPage.ts
+++ src/app/pages/login/LoginPage.ts
@@ -9,52 +9,60 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  password: string;
-  username!: string;
+  password: string = '';
+  username: string = '';
   message: string;
 
-  constructor(
-    private router: Router,
-    private loginService: LoginService,
-    private alertController: AlertController
-  ) {
-    this.password = '';
-    this.message = '';
-  }
+constructor(
+  private router: Router,
+  private loginService: LoginService,
+  private alertController: AlertController
+) {
+  this.password = '';
+  this.message = '';
+}
 
-  ngOnInit() {}
+ngOnInit() {}
 
-  async validateLogin() {
-    if (this.username.length >= 3 && this.username.length <= 8) {
-      if (this.password.toString().length === 5) {
-        const isValid = this.loginService.validateLogin(this.username, this.password.toString());
-        if (isValid) {
-          console.log("Login exitoso");
-          this.router.navigate(['/transition']);
-        } else {
-          console.log("No se pudo realizar el login");
-          this.presentAlert('Credenciales incorrectas');
-        }
+async validateLogin() {
+  if (this.username.length >= 3 && this.username.length <= 8) {
+    if (this.password && this.password.length === 5) {
+      const isValid = this.loginService.validateLogin(this.username, this.password.toString());
+      if (isValid) {
+        console.log("Login exitoso");
+        this.username = '';
+        this.password = '';
+        this.router.navigate(['/transition']);
       } else {
-        this.presentAlert("La contraseña debe tener 4 dígitos");
+        console.log("No se pudo realizar el login");
+        this.username = '';
+        this.password = '';
+        this.presentAlert('Credenciales incorrectas');
       }
     } else {
-      this.presentAlert("El nombre de usuario debe tener entre 3 y 8 caracteres");
+      this.presentAlert("La contraseña debe contener 5 dígitos");
+      this.username = '';
+        this.password = '';
     }
+  } else {
+    this.presentAlert("El nombre de usuario debe tener entre 3 y 8 caracteres");
+    this.username = '';
+    this.password = '';
   }
+}
 
-  async resetPassword() {
-    this.router.navigate(['/password'])
-  }
+async resetPassword() {
+  this.router.navigate(['/password'])
+}
 
 
-  async presentAlert(message: string) {
-    const alert = await this.alertController.create({
-      header: 'Error',
-      message: message,
-      buttons: ['OK']
-    });
+async presentAlert(message: string) {
+  const alert = await this.alertController.create({
+    header: 'Error',
+    message: message,
+    buttons: ['OK']
+  });
 
-    await alert.present();
-  }
+  await alert.present();
+}
 }
